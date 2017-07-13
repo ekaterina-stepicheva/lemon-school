fd230b13c1f9246edc990a1d8980aa5ce4bbf150
Ekaterina Stepicheva <e.stepicheva@gmail.com>
2017-07-13 23:37:23 +0300

    MyFolio

---
+++ MyFolio/script.js
@@ -0,0 +1,9 @@
+$('#burger-button').on('click', function() {
+	$('.burger-nav').toggleClass('active')
+});
+
+$(window).on('resize', function() {
+	if (window.innerWidth > 1024) {
+		$('.burger-nav').removeClass('active')
+	}
+});
